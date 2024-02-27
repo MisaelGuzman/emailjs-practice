a04a74a21cd5a9b8a0baad290792ce1f5cd2942b
MisaelGuzman <misaelguzman159@gmail.com>
2024-02-27 03:09:32 -0400

    Se comentó el codigo js

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
+//Importing Packages
 import { SMTPClient  } from 'emailjs'
 import 'dotenv/config'
 
+//Here call env variables
 const USER = process.env.USER;
 const PASSWORD = process.env.PASSWORD;
 const HOST = process.env.HOST;
 const EMAIL = process.env.EMAIL;
+
+// message subjec
 const sub = 'Stoic Quote'
+
+//empty variable
 let message;
 
+//Creating a new SMTPClient
 const client = new SMTPClient({
 	user: USER,
 	password: PASSWORD,
@@ -15,12 +22,14 @@ const client = new SMTPClient({
 	ssl: true,
 });
 
+//Getting a quote
 async function getQuote(){
     const response = await fetch('https://stoic.tekloon.net/stoic-quote');
     const script = await response.json();
     return script;
 }
 
+//Creating a message and sendind it
 async function quoteSender() {
     const {author, quote} = await getQuote();
     if (author.length === 0) {
@@ -29,8 +38,10 @@ async function quoteSender() {
 
     message = `${author} once time said: ${quote}`
 
+    //message sender
      client.send(
         {
+            //message extructure
             text: message,
             from: 'your email',
             to: 'Someone email',
